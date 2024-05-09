a53794979168d078cd8e689c705e49be16fc2b9a
Ondřej Slanař <slanar.ondrej@seznam.cz>
2024-05-09 17:56:32 +0200

    Conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,18 @@
 function ListGroup() {
+  let items = ["Praha", "Brno", "Olomouc", "Most", "Karviná"];
+  items = [];
+
+  items.map((item) => <li>{item}</li>);
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p></p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
