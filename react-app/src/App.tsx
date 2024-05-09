a2338d7911bfb9baba66fb3a1fd192d7972e54ab
Ondřej Slanař <slanar.ondrej@seznam.cz>
2024-05-09 18:48:21 +0200

    Passing data via props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,12 @@
 import ListGroup from "./components/ListGroup";
 function App() {
-  return <div><ListGroup/></div>
+  let items = ["Praha", "Brno", "Olomouc", "Most", "Karviná", "Kadaň"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />{" "}
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["Praha", "Brno", "Olomouc", "Most", "Karviná"];
+interface Props {
+  items: string[];
+  heading: string;
+}
+function ListGroup({ items, heading }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p></p>}
       <ul className="list-group">
         {items.map((item, index) => (
