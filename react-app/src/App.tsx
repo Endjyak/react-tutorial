97b06586ff1723bcc5ab52a1de848c3b5fd5afec
Ondřej Slanař <slanar.ondrej@seznam.cz>
2024-05-12 15:00:31 +0200

    Passing children

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,10 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
-  let items = ["Praha", "Brno", "Olomouc", "Most", "Karviná", "Kadaň"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />{" "}
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
